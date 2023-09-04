a210ed6a4666fbde7173b11ef407f2fdc27374fc
GGilook <rkfaorl1480@gmail.com>
2023-09-04 18:04:01 +0900

    feature2

---
+++ src/subtract.java
@@ -0,0 +1,9 @@
+public class subtract {
+
+    public static int subtractCal(int a, int b) {
+        return a - b;
+    }
+   public static void main(String[] args) {
+        System.out.println(subtractCal(10, 5));
+   } 
+}
